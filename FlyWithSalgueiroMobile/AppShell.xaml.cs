2c699a126dc63434d6a389af52ef3115144b1290
nunosalg <nunosalgueiro23@gmail.com>
2024-10-30 13:16:48 +0000

    Add project files.

---
+++ FlyWithSalgueiroMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlyWithSalgueiroMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FlyWithSalgueiroMobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FlyWithSalgueiroMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
